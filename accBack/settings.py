d79b06c9349b4c7e221bc54e38cf5cb263b4b791
Benjamin-Nyakambangwe <benjaminnyakambangwe@gmail.com>
2022-04-05 08:49:52 +0200

    changed email

--- accBack/settings.py
+++ accBack/settings.py
@@ -208,7 +208,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'malingreatsdev@gmail.com'
+EMAIL_HOST_USER = 'goodmarket.store@gmail.com'
 EMAIL_HOST_PASSWORD = '123456seven'
 EMAIL_USE_SSL = False
 
--- users/views.py
+++ users/views.py
@@ -232,8 +232,8 @@ class RequestPasswordResetEmail(generics.GenericAPIView):
             print('Begin')
             print(user)
             print('End')
-            send_mail('Reset Your Password', email_body, 'malingreatsdev@gmail.com',
-                      ['benjaminnyakambangwe@gmail.com', user.email], fail_silently=False)
+            send_mail('Reset Your Password', email_body, 'goodmarket.store@gmail.com',
+                      ['goodmarket.store@gmail.com', user.email], fail_silently=False)
             return Response({'success': 'We have sent you a link to reset your password'}, status=status.HTTP_200_OK)
 
 
